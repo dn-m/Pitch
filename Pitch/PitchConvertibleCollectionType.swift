d1c6284b398230cd6762c349758799c2b6571fc5
jsbean <jbean46@gmail.com>
2016-06-03 19:16:09 +0200

    Refactor, cleanup, doc comments

--- Pitch/PitchConvertibleCollectionType.swift
+++ Pitch/PitchConvertibleCollectionType.swift
@@ -8,26 +8,38 @@
 
 import ArrayTools
 
+/**
+ Ordered collection of non-unique `PitchConvertible`-conforming values.
+ */
 public protocol PitchConvertibleCollectionType: PitchConvertibleContaining, CollectionType {
     
-    associatedtype Element
+    // MARK: - Associated Types
     
+    /// `PitchConvertible`-conforming type of values contained herein.
+    associatedtype Element: PitchConvertible
+    
+    /// `Array` holding `PitchConvertible` values
     var array: Array<Element> { get }
 }
 
 extension PitchConvertibleCollectionType {
+
+    /// Iterable sequence of `Pitch` values contained herein.
+    public var sequence: AnySequence<Element> { return AnySequence(array) }
+}
+
+extension PitchConvertibleCollectionType {
     
+    /// - returns `true` if there are no `Pitch` values contained herein. Otherwise, `false`.
     public var isEmpty: Bool { return array.isEmpty }
+    
+    /// - returns `true` if there is one `Pitch` value contained herein. Otherwise `false`.
     public var isMonadic: Bool { return array.count == 1 }
 }
 
 extension PitchConvertibleCollectionType {
     
     // MARK: CollectionType
-    
-    // MARK: - Associated Types
-    
-    /// Pitch type contained here
     
     /// Start index
     public var startIndex: Int { return 0 }
@@ -39,4 +51,12 @@ extension PitchConvertibleCollectionType {
      - returns: `Pitch` value at the given `index`.
      */
     public subscript(index: Int) -> Element { return array[index] }
+}
+
+extension PitchConvertibleCollectionType {
+    
+    /// Printed description of `PitchConvertibleCollectionType`.
+    public var description: String {
+        return "〈\(map{ "\($0)" }.joinWithSeparator(","))〉"
+    }
 }
--- Pitch/PitchSequence.swift
+++ Pitch/PitchSequence.swift
@@ -10,20 +10,19 @@ import ArrayTools
 
 /**
  Ordered collection of non-unique `Pitch` values.
- 
- - TODO: inverse, retrograde, retrograde inverse, transposition at ...,
- 
  */
-public struct PitchSequence {
+public struct PitchSequence: PitchConvertibleCollectionType {
     
-    private let pitches: Array<Pitch>
+    public typealias Element = Pitch
+    
+    public let array: Array<Pitch>
     
     // TODO: prime form
     // TODO: inverse
     // TODO: optional : retrograde inverse
     // TODO: trasposedBy()
     
-    public var retrograde: PitchSequence { return PitchSequence(pitches.reverse()) }
+    public var retrograde: PitchSequence { return PitchSequence(array.reverse()) }
 }
 
 extension PitchSequence: ArrayLiteralConvertible {
@@ -33,8 +32,8 @@ extension PitchSequence: ArrayLiteralConvertible {
     /**
      Create a `PitchSequence` with an array literal.
      */
-    public init(arrayLiteral elements: Pitch...) {
-        self.pitches = elements
+    public init(arrayLiteral elements: Element...) {
+        self.array = elements
     }
 }
 
@@ -42,47 +41,11 @@ extension PitchSequence: PitchSequenceType {
     
     // MARK: - PitchSequenceType
     
-    /// Iterable sequence of `Pitch` values contained herein.
-    public var sequence: AnySequence<Pitch> { return AnySequence(pitches) }
-    
-    /// - returns: `true` if there is one `Pitch` object herein. Otherwsie `false`.
-    public var isMonadic: Bool { return count == 1 }
-    
     /**
      Create a `PitchSet` with `SequenceType` containing `Pitch` values.
      */
     public init<S: SequenceType where S.Generator.Element == Pitch>(_ sequence: S) {
-        self.pitches = Array(sequence)
+        self.array = Array(sequence)
     }
 }
 
-extension PitchSequence: CollectionType {
-    
-    // MARK: CollectionType
-    
-    // MARK: - Associated Types
-    
-    /// Pitch type contained here
-    public typealias Element = Pitch
-    
-    /// Start index
-    public var startIndex: Int { return 0 }
-    
-    /// End index
-    public var endIndex: Int { return pitches.count }
-    
-    /**
-     - returns: `Pitch` value at the given `index`.
-     */
-    public subscript(index: Int) -> Element { return pitches[index] }
-}
-
-extension PitchSequence: CustomStringConvertible {
-    
-    // MARK: - CustomStringConvertible
-    
-    /// Printed description of `PitchSequence`.
-    public var description: String {
-        return "〈\(map{ "\($0)" }.joinWithSeparator(","))〉"
-    }
-}
