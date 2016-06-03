7e847314106c0f20966304353c834bc91e7c19cb
jsbean <jbean46@gmail.com>
2016-06-03 19:08:36 +0200

    Add doc comments

--- Pitch/PitchConvertible.swift
+++ Pitch/PitchConvertible.swift
@@ -6,7 +6,13 @@
 //  Copyright © 2016 James Bean. All rights reserved.
 //
 
+/**
+ Conforming types can be initialized with a `Pitch` value.
+ */
 public protocol PitchConvertible: Hashable {
     
+    /**
+     Initialize conforming type with a `Pitch` value.
+     */
     init(_ pitch: Pitch)
 }
