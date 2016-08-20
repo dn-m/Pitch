951c13adcda246b14a98d81bf8b536f4d14bf3f8
jsbean <jbean46@gmail.com>
2016-08-20 11:03:16 -0400

    Refine PitchSequence.intervals

--- Pitch/PitchSequence.swift
+++ Pitch/PitchSequence.swift
@@ -19,11 +19,8 @@ public struct PitchSequence: PitchConvertibleCollectionType {
     public let array: Array<Pitch>
 
     /// - TODO: Make IntervalSequence
-    public var intervals: [Interval] {
-        return array.adjacentPairs?
-            .map(Dyad.init)
-            .map(Interval.init)
-            ?? []
+    public var intervals: [Interval]? {
+        return array.adjacentPairs?.lazy.map(Dyad.init).map(Interval.init)
     }
 }
 
