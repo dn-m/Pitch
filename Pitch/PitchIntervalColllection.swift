0c38a7215527194fa2887f856e4aece937b17d79
jsbean <jbean46@gmail.com>
2016-12-02 10:38:53 -0500

    Refine doc comments

--- Pitch/PitchIntervalColllection.swift
+++ Pitch/PitchIntervalColllection.swift
@@ -8,20 +8,24 @@
 
 import ArrayTools
 
+/// Collection of `PitchInterval` values
 public struct PitchIntervalCollection: NoteNumberRepresentableCollectionType {
     
+    /// `NoteNumberRepresentable` type of dyad.
     public typealias Dyad = PitchDyad
     
+    /// `NoteNumberRepresentable` type of interval.
     public typealias Interval = PitchInterval
     
+    // MARK: - Instance Properties
+    
+    /// Backing store of elements contained herein.
     public let array: Array<Element>
 }
 
 extension PitchIntervalCollection: AnySequenceType {
     
-    // MARK: - PitchSequenceType
-    
-    // MARK: - Associated Types
+    // MARK: - PitchIntervalCollection
     
     /// `PitchConvertible` values contained herein.
     public typealias Element = PitchInterval
@@ -48,12 +52,16 @@ extension PitchIntervalCollection: ExpressibleByArrayLiteral {
 
 extension PitchIntervalCollection: Sequence {
     
+    // MARK: - Sequence
+    
+    /// Make an iterator for a `PitchIntervalCollection`.
     public func makeIterator() -> AnyIterator<Element> {
         let iterator = sequence.makeIterator()
         return AnyIterator { iterator.next() }
     }
 }
- 
+
+/// - returns: `true` if all elements in both collections are equivalent. Otherwise, `false`.
 public func == (lhs: PitchIntervalCollection, rhs: PitchIntervalCollection) -> Bool {
     return lhs.sequence == rhs.sequence
 }
