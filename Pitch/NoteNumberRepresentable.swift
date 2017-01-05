78bdbfe65a5c631833b2104caa6bf7eb57d9df32
James <jbean46@gmail.com>
2017-01-05 15:55:12 +0100

    Rearrange things for joy

--- Pitch/NoteNumberRepresentable.swift
+++ Pitch/NoteNumberRepresentable.swift
@@ -32,16 +32,28 @@ extension NoteNumberRepresentable {
     }
 }
 
-/// - returns: `true` if both values are representable by the same `NoteNumber`.
-/// Otherwise, `false`.
-public func == <T: NoteNumberRepresentable> (lhs: T, rhs: T) -> Bool {
-    return lhs.noteNumber == rhs.noteNumber
+extension NoteNumberRepresentable {
+    
+    // MARK: - `Equatable`
+    
+    /// - returns: `true` if both values are representable by the same `NoteNumber`.
+    /// Otherwise, `false`.
+    public func == <T: NoteNumberRepresentable> (lhs: T, rhs: T) -> Bool {
+        return lhs.noteNumber == rhs.noteNumber
+    }
 }
 
-/// - returns: `true` if the first value is less than the seconds value. Otherwise, `false`.
-public func < <T: NoteNumberRepresentable> (lhs: T, rhs: T) -> Bool {
-    return lhs.noteNumber < rhs.noteNumber
+extension Comparable {
+    
+    // MARK: - `Comparable`
+    
+    /// - returns: `true` if the first value is less than the second value. Otherwise, `false`.
+    public func < <T: NoteNumberRepresentable> (lhs: T, rhs: T) -> Bool {
+        return lhs.noteNumber < rhs.noteNumber
+    }
 }
+
+// MARK: - Transposition
 
 /// - returns: A `NoteNumberRepresentable` value that is the difference between the two given
 /// values.
